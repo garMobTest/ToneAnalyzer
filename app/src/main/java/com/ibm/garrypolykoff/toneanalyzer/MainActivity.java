dc628263306e2bca7dedcec6fd19ec5a190000fc
garrypolykoff <gpolyko@us.ibm.com>
2017-02-06 17:40:37 -0500

    grid layout to display results according to Nax's sketch .

--- app/src/main/java/com/ibm/garrypolykoff/toneanalyzer/MainActivity.java
+++ app/src/main/java/com/ibm/garrypolykoff/toneanalyzer/MainActivity.java
@@ -1,32 +1,33 @@
 package com.ibm.garrypolykoff.toneanalyzer;
 
-import android.app.Activity;
+
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.SentenceTone;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneCategory;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneScore;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
 
 
 import java.util.Iterator;
@@ -40,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
     private TextView analsisTextView;
     private Button analysisButton;
     private Map valuesMap = new HashMap();
+
+    private final String  LIKELY = "Likely";
+    private final  String VERYLIKELY = "Very Likely";
+    private final  String UNLIKELY = "Unlikely";
+    private GridLayout gridView;
+
+    private final int ColorUnlikely = Color.GRAY;
+    private final int ColorLikely = Color.YELLOW;
+    private final int ColorVeryLikely = Color.RED;
 
 
     /**
@@ -64,7 +74,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         analysisButton = (Button) findViewById(R.id.sendAnalysisButton);
-        analsisTextView = (TextView) findViewById(R.id.analysisTextView);
+     //   analsisTextView = (TextView) findViewById(R.id.analysisTextView);
+        gridView = (GridLayout) findViewById(R.id.gridView);
+        gridView.setRowCount(5);
+        gridView.setColumnCount(2);
+
 
         analysisButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -108,8 +122,14 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Param", params[0]);
             String value = params[0];
 
+          //  tone.addSentencesTone(sentenceTone);
+
             // Call the service and get the tone
-            tone = service.getTone(value, null).execute();
+            tone = service.getTone(value,null ).execute();
+
+
+
+
             success = true;
 
 
@@ -130,35 +150,120 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
 
+            TextView viewEmotions;
+            TextView viewScore;
+            gridView.removeAllViews();
+            int probColor;
+
             //this method will be running on UI thread
             if (success) {
                 pdLoading.dismiss();
                 valuesMap.clear();
                 for (ToneCategory tc : tone.getDocumentTone().getTones())
-                    for (ToneScore ts : tc.getTones()) {
 
-                        valuesMap.put(ts.getName(), ts.getScore());
+                    if (tc.getId().equals("emotion_tone")) {
+                        for (ToneScore ts : tc.getTones()) {
+
+
+                            valuesMap.put(ts.getName(), ts.getScore());
+
+
+                        }
 
                     }
 
                 String toneScore = "";
 
+
+
+
                 Iterator entries = valuesMap.entrySet().iterator();
                 while (entries.hasNext()) {
                     Map.Entry thisEntry = (Map.Entry) entries.next();
                     Log.e("Entry", (String) thisEntry.getKey());
-                    toneScore += thisEntry.getKey() + ": " + thisEntry.getValue() + "\n";
+
+                    String probability;
+
+                    if ((double)thisEntry.getValue() < 0.3) {
+
+                        probability = UNLIKELY;
+
+                        probColor = ColorUnlikely;
+
+
+
+                    } else if ((double)thisEntry.getValue() > 0.3 && (double)thisEntry.getValue() < 0.6) {
+
+                        probability = LIKELY;
+                        probColor = ColorLikely;
+
+
+
+
+                    } else {
+
+                        probability = VERYLIKELY;
+                        probColor = ColorVeryLikely;
+                    }
+
+                    viewEmotions = new TextView(getApplicationContext());
+                    viewScore = new TextView(getApplicationContext());
+
+
+                    viewEmotions.setText((CharSequence) thisEntry.getKey() + ": ");
+                    viewEmotions.setTextColor(Color.BLACK);
+                    viewEmotions.setTextSize(20f);
+
+
+                    viewScore.setText((CharSequence)probability);
+                    viewScore.setTextColor(probColor);
+                    viewScore.setTextSize(20f);
+
+
+
+                   // toneScore += thisEntry.getKey() + ": " + probability + "\n";
+
+
+
+                    gridView.addView(viewEmotions);
+
+                    GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+
+                    param.setGravity(Gravity.LEFT);
+
+                    viewEmotions.setLayoutParams (param);
+
+
+
+                    gridView.addView(viewScore);
+
+                    GridLayout.LayoutParams param1 =new GridLayout.LayoutParams();
+
+                    param1.setGravity(Gravity.CENTER);
+                    viewScore.setLayoutParams(param1);
 
                 }
 
 
-                analsisTextView.setText(toneScore, TextView.BufferType.SPANNABLE);
+              //  analsisTextView.setText(toneScore, TextView.BufferType.SPANNABLE);
 
             }
         }
 
     }
 
+    /**
+    GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+    param.height = LayoutParams.WRAP_CONTENT;
+    param.width = LayoutParams.WRAP_CONTENT;
+    param.rightMargin = 5;
+    param.topMargin = 5;
+    param.setGravity(Gravity.CENTER);
+    param.columnSpec = GridLayout.spec(c);
+    param.rowSpec = GridLayout.spec(r);
+    titleText.setLayoutParams (param);
+
+     **/
 
 
     @Override
